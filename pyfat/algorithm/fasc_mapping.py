817412522ed0cc28067baa017ec9529906fb3c43
zhenzonglei <zhenzonglei@gmail.com>
2017-09-09 09:57:25 +0800

    RF:fasciculus mapping

---
+++ pyfat/algorithm/fasc_mapping.py
@@ -0,0 +1,10 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:l
+
+
+"""
+The module consits of functions which map fasciculus information to brain volume or cortical surface, including,
+endpoint mapping,density mapping, midsag plane mapping. 
+"""
