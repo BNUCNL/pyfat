d5e0f51f9f1139a948e6347862b300a7bec9fbe7
jiangjiae <jiangjiane@outlook.com>
2017-09-29 19:53:43 +0800

    add some metric

--- pyfat/algorithm/metric.py
+++ pyfat/algorithm/metric.py
@@ -3,6 +3,9 @@
 
 
 from scipy.spatial.distance import pdist, squareform
+from dipy.tracking.utils import length
+import nibabel.streamlines.tck as nibtck
+import nibabel.streamlines.array_sequence as nibAS
 
 
 def coordinate_dist(coordinate, metric='euclidean'):
@@ -23,3 +26,22 @@ def coordinate_dist(coordinate, metric='euclidean'):
     dist_temp = pdist(coordinate, metric)
     sdist = squareform(dist_temp)
     return sdist
+
+
+class Metric(object):
+    def __init__(self, imgtck):
+        self.imgtck = imgtck
+        self.lengths = None
+        self.count = None
+
+    def length(self):
+        self.lengths = length(self.imgtck)
+        return self.lengths
+
+    def count(self):
+        if isinstance(self.imgtck, nibtck.TckFile):
+            self.count = len(self.imgtck.streamlines)
+        if isinstance(self.imgtck, nibAS.ArraySequence):
+            self.count = len(self.imgtck)
+        return self.count
+            
