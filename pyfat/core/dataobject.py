b75773ba0c11063f5eb9d4408ffbe5e1de715f63
jiangjiae <jiangjiane@outlook.com>
2017-11-01 12:55:59 +0800

    add save2tck

--- pyfat/core/dataobject.py
+++ pyfat/core/dataobject.py
@@ -2,22 +2,46 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import nibabel.streamlines.tractogram as nibtcg
 from dipy.tracking.utils import length
 import nibabel.streamlines.tck as nibtck
 
 class Fasciculus(object):
     """Base attributes of the fasciculus"""
-    def __init__(self, source, header=None, lengths_min=None, lengths_max=None):
+    def __init__(self, source, header=None, lengths_min=None, lengths_max=None,
+                 data_per_streamline=None, data_per_point=None, affine_to_rasmm=None):
         """
         Create a dataset  from an TckFile which has following attributes:
 
         Parameters
         ----------
-        source: Tck file path or ArraySequence
+        source : Tck file path or ArraySequence
             Tck dataset, specified either as a filename (single file) or
             a ArraySequence. When source is a ArraySequence, parameter header is required.
-        header: Tck header structure
+        header : Tck header structure
             Tck header structure.
+        lengths_min : Fasciculus min length
+            Fasciculus min length
+        lengths_max : Faciculus max length
+            Faciculus max length
+        data_per_streamline : dict of iterable of ndarrays, optional
+            Dictionary where the items are (str, iterable).
+            Each key represents an information $i$ to be kept alongside every
+            streamline, and its associated value is an iterable of ndarrays of
+            shape ($P_i$,) where $P_i$ is the number of scalar values to store
+            for that particular information $i$.
+        data_per_point : dict of iterable of ndarrays, optional
+            Dictionary where the items are (str, iterable).
+            Each key represents an information $i$ to be kept alongside every
+            point of every streamline, and its associated value is an iterable
+            of ndarrays of shape ($N_t$, $M_i$) where $N_t$ is the number of
+            points for a particular streamline $t$ and $M_i$ is the number
+            scalar values to store for that particular information $i$.
+        affine_to_rasmm : ndarray of shape (4, 4) or None, optional
+            Transformation matrix that brings the streamlines contained in
+            this tractogram to *RAS+* and *mm* space where coordinate (0,0,0)
+            refers to the center of the voxel. By default, the streamlines
+            are in an unknown space, i.e. affine_to_rasmm is None.
 
         Return
         ------
@@ -33,10 +57,27 @@ class Fasciculus(object):
                 self._img = None
             else:
                 raise ValueError("Data dimension does not match.")
+            if isinstance(data_per_streamline, nibtcg.PerArrayDict):
+                self._data_per_streamline = data_per_streamline
+            else:
+                raise ValueError("data_per_streamline must be a object of "
+                                 "class nibabel.streamlines.tractogram.PerArrayDict.")
+            if isinstance(data_per_point, nibtcg.PerArraySequenceDict):
+                self._data_per_point = data_per_point
+            else:
+                raise ValueError("data_per_point must be a object of "
+                                 "class nibabel.streamlines.tractogram.PerArraySequenceDict.")
+            if affine_to_rasmm is None:
+                self._affine_to_rasmm = self._header['voxel_to_rasmm']
+            else:
+                self._affine_to_rasmm = affine_to_rasmm
         else:
             self._img = nibtck.TckFile.load(source)
             self._header = self._img.header
             self._data = self._img.streamlines
+            self._data_per_streamline = self._img.tractogram.data_per_streamline
+            self._data_per_point = self._img.tractogram.data_per_point
+            self._affine_to_rasmm = self._header['voxel_to_rasmm']
 
         # tractography algorithm
         self._algorithm = self._header['method']
@@ -48,6 +89,10 @@ class Fasciculus(object):
         self._lengths = self.get_lengths()
         # counts
         self._counts = self.get_counts()
+        # labes
+        self._labels = len(self._data)*[None]
+        # dict(self._label, self._data)
+        self._labels_data = dict(zip(self._labels, self._data))
 
         if lengths_min is None:
             self._lengths_min = self.get_lengths_min()
@@ -92,8 +137,8 @@ class Fasciculus(object):
     def set_lengths_min(self, min_value):
         try:
             min_value = float(min_value)
-            self._lengths_min = min_value
             if self.get_lengths_min() <= min_value <= self.get_lengths_max():
+                self._lengths_min = min_value
                 index = self._lengths >= min_value
                 self._data = self._data[index]
             else:
@@ -103,10 +148,19 @@ class Fasciculus(object):
 
     def set_lengths_max(self, max_value):
         try:
+            max_value = float(max_value)
             if self.get_lengths_min() <= max_value <= self.get_lengths_max():
+                self._lengths_max = max_value
                 index = self._lengths <= max_value
                 self._data = self._data[index]
             else:
                 ValueError("max_value is not in the range of length_min to length_max.")
         except ValueError:
             print "min_value must be a number."
+
+    def save2tck(self, file_path):
+        """Save to a tck file"""
+        tractogram = nibtcg.Tractogram(streamlines=self._data, data_per_streamline=self._data_per_streamline,
+                                       data_per_point=self._data_per_point, affine_to_rasmm=self._affine_to_rasmm)
+        datdat = nibtck.TckFile(tractogram=tractogram, header=self._header)
+        datdat.save(file_path)
