491518932cb8dd1ec6d9165f5a1eada46c646794
jiangjiae <jiangjiane@outlook.com>
2017-11-07 21:38:21 +0800

    add terminus2surface

--- pyfat/algorithm/fasc_mapping.py
+++ pyfat/algorithm/fasc_mapping.py
@@ -10,7 +10,9 @@ endpoint mapping,density mapping, midsag plane mapping.
 """
 
 import nibabel as nib
+from scipy.spatial.distance import cdist
 import dipy.tracking.utils as ditu
+import nibabel.streamlines.array_sequence as nibas
 
 # fiber density of volume
 def fib_density_map(volume, fiber, output_path):
@@ -38,3 +40,15 @@ def fib_density_map(volume, fiber, output_path):
     dm_img = nib.Nifti1Image(image_volume.astype("int16"), affine)
     dm_img.to_filename(output_path)
 
+def terminus2surface(streamlines, geo_path, scalar_path=None):
+    """Points project surface"""
+    coords, faces = nib.freesurfer.read_geometry(geo_path)
+    scalar_data = nib.freesurfer.read_morph_data(scalar_path)
+    s0 = [s[0] for s in streamlines]
+    s_t = [s[-1] for s in streamlines]
+    s = s0 + s_t
+    stream_terminus = nibas.ArraySequence(s)
+    y = cdist(stream_terminus, coords)
+    nearest_coords = nibas.ArraySequence([coords[y[i].argmin()] for i in range(len(y[:]))])
+
+    return stream_terminus, nearest_coords
