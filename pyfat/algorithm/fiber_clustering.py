d61ab23739d4bea1e5e2a3aff8dd3ff15d48e6ae
jiangjiae <jiangjiane@outlook.com>
2017-11-07 21:39:42 +0800

    add bundle_thre_seg bundle_centriods terminus_symmetry

--- pyfat/algorithm/fiber_clustering.py
+++ pyfat/algorithm/fiber_clustering.py
@@ -4,10 +4,12 @@
 # vi: set ft=python sts=4 ts=4 sw=4 et:l
 
 import numpy as np
+from math import sqrt, pow
 from dipy.segment.quickbundles import QuickBundles
 import nibabel.streamlines.array_sequence as nibas
 
 from pyfat.core.dataobject import Fasciculus
+from pyfat.viz.visualization import show
 
 
 class FibClustering(object):
@@ -40,6 +42,7 @@ class FibClustering(object):
         index_term = np.vstack((np.array([index0]), np.array([index1]))).sum(axis=0)
         index = index_term > 1
         length_seg_data = fasciculus_data[index]
+
         return index, length_seg_data
 
     def length_seg(self):
@@ -88,12 +91,52 @@ class FibClustering(object):
 
         return labels, length_clusters
 
-    def bundle_seg(self, streamlines, dist_thre=20.0, pts=12):
+    def bundle_seg(self, streamlines, dist_thre=10.0, pts=12):
         """QuickBundles-based segmentation"""
         bundles = QuickBundles(streamlines, dist_thre, pts)
         clusters = bundles.clusters()
         labels = np.array(len(streamlines) * [None])
+        N_list = []
+        for i in range(len(clusters)):
+            N_list.append(clusters[i]['N'])
+        # show(N_list, title='N histogram', xlabel='N')
+        data_clusters = []
         for i in range(len(clusters)):
             labels[clusters[i]['indices']] = i + 1
+            data_clusters.append(streamlines[clusters[i]['indices']])
 
-        return labels
+        return labels, data_clusters, N_list
+
+    def bundle_thre_seg(self, streamlines, cluster_thre=10, dist_thre=10.0, pts=12):
+        """QuickBundles-based segmentation"""
+        bundles = QuickBundles(streamlines, dist_thre, pts)
+        bundles.remove_small_clusters(cluster_thre)
+        clusters = bundles.clusters()
+        data_clusters = []
+        for key in clusters.keys():
+            data_clusters.append(streamlines[clusters[key]['indices']])
+
+        return data_clusters
+
+    def bundle_centroids(self, streamlines, cluster_thre=10, dist_thre=10.0, pts=12):
+        """QuickBundles-based segmentation"""
+        bundles = QuickBundles(streamlines, dist_thre, pts)
+        bundles.remove_small_clusters(cluster_thre)
+        centroids = bundles.centroids
+
+        return nibas.ArraySequence(centroids)
+
+    def terminus_symmetry(self, streamlines, dist_thre=5.0):
+        dist = np.array([sqrt(pow(abs(s[0][0])-abs(s[-1][0]), 2) +
+                              pow(s[0][1]-s[-1][1], 2) + pow(s[0][2]-s[-1][2], 2))
+                         for s in streamlines])
+        index = dist < dist_thre
+        term_sym_streamlines = streamlines[index]
+
+        return term_sym_streamlines
+
+
+
+    def voxel_seg(self):
+        """Voxel-based clustering"""
+        pass
