b4345d3cf1aea3494feb95984d1a0a58baded3cd
jiangjiae <jiangjiane@outlook.com>
2017-11-23 15:35:40 +0800

    remove if __name__ == '__main__'

--- pyfat/interfaces/gifti2geometry.py
+++ pyfat/interfaces/gifti2geometry.py
@@ -11,12 +11,3 @@ def gii2geo(geo_gii, geo_path):
     gii_data = nib.load(geo_gii).darrays
     coords, faces = gii_data[0].data, gii_data[1].data
     nib.freesurfer.write_geometry(geo_path, coords, faces)
-
-
-if __name__ == '__main__':
-    geo_gii = '/home/brain/workingdir/data/dwi/hcp/preprocessed/' \
-              'response_dhollander/100408/Native/100408.L.inflated.native.surf.gii'
-    geo_path = '/home/brain/workingdir/data/dwi/hcp/preprocessed/' \
-              'response_dhollander/100408/Native/surf/lh.inflated'
-
-    gii2geo(geo_gii, geo_path)
