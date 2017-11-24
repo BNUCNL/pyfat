54d32a416b600fd878e12de7e883a2d50c48d7ff
jiangjiae <jiangjiane@outlook.com>
2017-11-24 10:20:23 +0800

    cc_seg

---
+++ pyfat/algorithm/cc_seg.py
@@ -0,0 +1,28 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import nibabel as nib
+import numpy.linalg as npl
+from nibabel.affines import apply_affine
+
+from pyfat.core.dataobject import Fasciculus
+
+
+def cc_seg_region(vol_path, roi_fib_paths_suffix):
+    """
+    Segment cc according to roi fibs
+    """
+    img = nib.load(vol_path)
+    counts = np.zeros(img.shape)
+    roi_name_list = os.listdir(roi_fib_paths_suffix)
+    for roi_i in range(len(roi_name_list)):
+        files = os.listdir(os.path.join(roi_fib_paths_suffix, roi_name_list[roi_i]))
+        for f in files:
+            roi_fib_path = os.path.join(roi_fib_paths_suffix, roi_name_list[roi_i], f)
+            fibs = apply_affine(npl.inv(img.affine), Fasciculus(roi_fib_path).xmin_nodes())
+            for n in fibs:
+                counts[int(n[0]), int(n[1]), int(n[2])] = roi_i + 1
+
+    return counts
