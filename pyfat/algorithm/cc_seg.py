deb8665134b4d6a19744dddd4dce2168c6fc54b1
jiangjiae <jiangjiane@outlook.com>
2017-11-25 15:08:47 +0800

    cc_seg according to a region

--- pyfat/algorithm/cc_seg.py
+++ pyfat/algorithm/cc_seg.py
@@ -18,6 +18,22 @@ def cc_seg_region(vol_path, roi_fib_paths_suffix):
     counts = np.zeros(img.shape)
     roi_name_list = os.listdir(roi_fib_paths_suffix)
     for roi_i in range(len(roi_name_list)):
+        roi_fib_path = os.path.join(roi_fib_paths_suffix, roi_name_list[roi_i])
+        fibs = apply_affine(npl.inv(img.affine), Fasciculus(roi_fib_path).xmin_nodes())
+        for n in fibs:
+            counts[int(n[0]), int(n[1]), int(n[2])] = roi_i + 1
+
+    return counts
+
+
+def cc_seg_regions(vol_path, roi_fib_paths_suffix):
+    """
+    Segment cc according to roi fibs
+    """
+    img = nib.load(vol_path)
+    counts = np.zeros(img.shape)
+    roi_name_list = os.listdir(roi_fib_paths_suffix)
+    for roi_i in range(len(roi_name_list)):
         files = os.listdir(os.path.join(roi_fib_paths_suffix, roi_name_list[roi_i]))
         for f in files:
             roi_fib_path = os.path.join(roi_fib_paths_suffix, roi_name_list[roi_i], f)
