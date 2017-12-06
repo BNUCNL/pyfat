b415cdf651c3060e72b8a990e5833b5bd58379d2
jiangjiae <jiangjiane@outlook.com>
2017-12-06 22:17:13 +0800

    add endpoints_axis2cc

--- pyfat/algorithm/cc_seg.py
+++ pyfat/algorithm/cc_seg.py
@@ -64,3 +64,40 @@ def cc_seg_same_regions(vol_path, lr_region_path_suffix):
                 counts[int(m[0]), int(m[1]), int(m[2])] += 1
 
     return counts
+
+
+def endpoints_axis2cc(vol_path, streamlines_path, axis='y', mode='max'):
+    """
+    Endpoints of streamlines mapping to cc.
+    axis: 'x', 'y', 'z'
+    mode: 'max', 'min', 'mean'
+    """
+    img = nib.load(vol_path)
+    counts_l = np.zeros(img.shape)
+    counts_r = np.zeros(img.shape)
+    fibs_points = apply_affine(npl.inv(img.affine), Fasciculus(streamlines_path).xmin_nodes()).astype(int)
+    streamlines = Fasciculus(streamlines_path).sort_streamlines()
+    if axis == 'x':
+        endpoints_l = np.array([fib[0][0] for fib in streamlines])
+        endpoints_r = np.array([fib[-1][0] for fib in streamlines])
+    elif axis == 'y':
+        endpoints_l = np.array([fib[0][1] for fib in streamlines])
+        endpoints_r = np.array([fib[-1][1] for fib in streamlines])
+    else:
+        endpoints_l = np.array([fib[0][2] for fib in streamlines])
+        endpoints_r = np.array([fib[-1][2] for fib in streamlines])
+    for i in range(len(streamlines)):
+        index_i = fibs_points == np.array([fibs_points[i][0], fibs_points[i][1], fibs_points[i][2]])
+        index_i = index_i.sum(axis=1)
+        inddex = index_i == 3
+        if mode == 'max':
+            counts_l[fibs_points[i][0], fibs_points[i][1], fibs_points[i][2]] = endpoints_l[inddex].max()
+            counts_r[fibs_points[i][0], fibs_points[i][1], fibs_points[i][2]] = endpoints_r[inddex].max()
+        elif mode == 'min':
+            counts_l[fibs_points[i][0], fibs_points[i][1], fibs_points[i][2]] = endpoints_l[inddex].min()
+            counts_r[fibs_points[i][0], fibs_points[i][1], fibs_points[i][2]] = endpoints_r[inddex].min()
+        else:
+            counts_l[fibs_points[i][0], fibs_points[i][1], fibs_points[i][2]] = endpoints_l[inddex].mean()
+            counts_r[fibs_points[i][0], fibs_points[i][1], fibs_points[i][2]] = endpoints_r[inddex].mean()
+
+    return counts_l, counts_r
