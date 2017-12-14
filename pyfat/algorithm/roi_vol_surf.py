ad5975e4a3e7e8f9206056649cc686bfc51aaa38
jiangjiae <jiangjiane@outlook.com>
2017-12-14 20:29:02 +0800

    add roi_surf2vol

--- pyfat/algorithm/roi_vol_surf.py
+++ pyfat/algorithm/roi_vol_surf.py
@@ -85,3 +85,32 @@ def label2surf_roi(label, geo_path):
     r_label_value[r_label_index] = 1
 
     return l_label_value, r_label_value
+
+
+def roi_surf2vol(roi_surf_path, geo_path, vol_path):
+    """Transform rois from surface to volume."""
+    # load parcelltion data
+    vertices, colortable, label = nib.freesurfer.read_annot(roi_surf_path)
+
+    # load surf geometry
+    suffix = os.path.split(geo_path)[1].split('.')[-1]
+    if suffix in ('white', 'inflated', 'pial'):
+        coords, faces = nib.freesurfer.read_geometry(geo_path[0])
+    elif suffix == 'gii':
+        gii_data = nib.load(geo_path).darrays
+        coords, faces = gii_data[0].data, gii_data[1].data
+    else:
+        raise ImageFileError('This file format-{} is not supported at present.'.format(suffix))
+
+    # load volume data
+    img = nib.load(vol_path)
+    data_temp = np.zeros(img.shape)
+
+    coords_affine = apply_affine(npl.inv(img.affine), coords)
+
+    # parcellation of surf to volume
+    for value in set(vertices):
+        for c in coords_affine[vertices == value]:
+            data_temp[int(c[0]), int(c[1]), int(c[2])] = value
+
+    return data_temp
