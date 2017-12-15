97d37ade39187707905d1b0739bef041df98945b
jiangjiae <jiangjiane@outlook.com>
2017-12-15 20:45:06 +0800

    add random colors

--- pyfat/viz/colormap.py
+++ pyfat/viz/colormap.py
@@ -308,3 +308,16 @@ def create_colormap(v, name='plasma', auto=True):
     rgba = colormap(v)
     rgb = rgba[:, :3].copy()
     return rgb
+
+
+def create_random_colormap(v, rand_range=(0, 255)):
+    """
+    Create random colormap.
+    """
+    rgb = np.zeros([v, 3])
+    for i in range(v):
+        rgb[i, 0] = np.random.randint(rand_range[0], rand_range[1])
+        rgb[i, 1] = np.random.randint(rand_range[0], rand_range[1])
+        rgb[i, 2] = np.random.randint(rand_range[0], rand_range[1])
+
+    return rgb
